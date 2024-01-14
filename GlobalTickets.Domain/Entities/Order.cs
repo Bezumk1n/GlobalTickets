83004785fd70882be268a1d31186bd5030b7e3a7
Umnov Stanislav <umnov.msk@gmail.com>
2024-01-14 20:51:37 +0300

    Added base entities

---
+++ GlobalTickets.Domain/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using GlobalTickets.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTickets.Domain.Entities
+{
+    public class Order : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
