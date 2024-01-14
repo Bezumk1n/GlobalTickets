1cd03e9ab7c68528471a7826b19af37739196d78
Umnov Stanislav <umnov.msk@gmail.com>
2024-01-14 22:25:15 +0300

    Added: - AutoMapper - MediatR - Queries for Event feature

---
+++ GlobalTickets.Application/Features/Events/Queries/EventVm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTickets.Application.Features.Events.Queries
+{
+    public class EventVm
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public string ImageUrl { get; set; }= string.Empty;
+    }
+}
