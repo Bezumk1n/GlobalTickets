3c18cae6c29bb317dfbc261b4ef329cbe1eceb30
Umnov Stanislav <umnov.msk@gmail.com>
2024-01-14 22:25:15 +0300

    Added: - AutoMapper - MediatR - Queries for Event feature

---
+++ GlobalTickets.Application/Features/Events/Queries/GetEventsListQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTickets.Application.Features.Events.Queries
+{
+    public class GetEventsListQuery : IRequest<List<EventVm>>
+    {
+    }
+}
---
+++ GlobalTickets.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using GlobalTickets.Application.Features.Events.Queries;
+using GlobalTickets.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTickets.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Event, EventVm>().ReverseMap();
+            CreateMap<Event, EventDetailVm>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+        }
+    }
+}
