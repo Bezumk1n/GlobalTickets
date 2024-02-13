7456f7aa1255a7d2c17fb9e7417bd551330289ff
Umnov Stanislav <umnov.msk@gmail.com>
2024-02-13 22:35:21 +0300

    Added CRUD repository and other repositories

---
+++ GlobalTickets.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,81 @@
+﻿using GlobalTickets.Application.Interfaces.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTickets.Persistence.Repositories
+{
+    public class BaseRepository<T> : IAsyncRepository<T> where T : class
+    {
+        protected readonly GlobalTicketsDbContext _dbContext;
+        public BaseRepository(GlobalTicketsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        #region Base CRUD
+
+        /// <summary>
+        /// Create
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+        /// <summary>
+        /// Get entity by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+        /// <summary>
+        /// Get collection of enttities
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+        /// <summary>
+        /// Update entity
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+        /// <summary>
+        /// Delete entity
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+        #endregion
+
+
+
+
+
+
+    }
+}
---
+++ GlobalTickets.Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,23 @@
+﻿using GlobalTickets.Application.Interfaces.Persistence;
+using GlobalTickets.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTickets.Persistence.Repositories
+{
+    public class EventRepository : BaseRepository<Event>, IEventRepository
+    {
+        public EventRepository(GlobalTicketsDbContext dbContext) : base(dbContext)
+        {
+        }
+        public Task<bool> IsEventNameAndDateUnique(string name, DateTime eventDate)
+        {
+            var matches = _dbContext.Events.Any(e => e.Name == name && e.Date == eventDate);
+
+            return Task.FromResult(matches);
+        }
+    }
+}
